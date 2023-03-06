9db8069a19575541e6a1a9b28cd855665fac7ca1
Amirhosein80 <amir1380feiz@gmail.com>
2023-03-06 09:58:24 +0330

    Add __init__ :)

---
+++ models/__init__.py
@@ -0,0 +1,3 @@
+from models.bisenetv2 import BisenetV2
+from models.model_utils import convnext_init_weights, efficientnet_init_weights, regnet_init_weights,\
+    reparameterize_model, replace_relu_with_gelu, replace_relu_with_silu
