8f6ce775b9006fd4e03520558bcbc615127b2ba3
unknown <amir1380feiz@gmail.com>
2023-03-07 16:18:30 +0330

    Add details :)

--- utils/model_ema.py
+++ utils/model_ema.py
@@ -6,11 +6,15 @@ from typing import Optional
 
 class ModelEma(nn.Module):
     """
-    Model Exponential Moving Average
+    Model Exponential Moving Average :)
     Code from: https://github.com/huggingface/pytorch-image-models/blob/main/timm/utils/model_ema.py
     """
-
     def __init__(self, model: nn.Module, decay: float = 0.9999, device: Optional = None):
+        """
+        :param: model: your model
+        :param decay: decat parameter for teacher and student weights. default is 0.9999
+        :param device: device for teacher model. default is None
+        """
         super().__init__()
         self.module = copy.deepcopy(model)
         self.module.eval()
@@ -20,6 +24,10 @@ class ModelEma(nn.Module):
             self.module.to(device=device)
 
     def _update(self, model):
+        """
+        update function for student weights :)
+        :param model: student model
+        """
         with torch.no_grad():
             for ema_v, model_v in zip(self.module.state_dict().values(), model.state_dict().values()):
                 if self.device is not None:
@@ -27,6 +35,11 @@ class ModelEma(nn.Module):
                 ema_v.copy_(self._update_fn(ema_v, model_v))
 
     def _update_fn(self, e, m):
+        """
+        calculate teacher weight:)
+        :param e: teacher weights
+        :param m: student weights
+        """
         return self.decay * e + (1. - self.decay) * m
 
     def update(self, model):
