152d05af82923bffee037e7dffda9d19d2639508
Amirhosein80 <amir1380feiz@gmail.com>
2023-03-10 11:41:59 +0330

    Finish train & eval fuctions :)

--- utils/trainer.py
+++ utils/trainer.py
@@ -1,18 +1,32 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as f
-import torch.optim as optim
-import torch.cuda.amp as amp
+import copy
 import datetime
 import os
-
-from utils.model_ema import ModelEma
-from loss_funcs import BootstrappedCE, FocalLoss
 from collections import OrderedDict
+
+import tensorboardX as ts
+import torch
+import torch.cuda.amp as amp
+import torch.nn as nn
+import torch.optim as optim
+import tqdm.autonotebook as tqdm
+
+from loss_funcs import BootstrappedCE, FocalLoss
+from models.model_utils import reparameterize_model
+from utils.model_ema import ModelEma
+from utils.train_utils import ConfusionMatrix, AverageMeter, set_seed, get_lr
 
 
 class Trainer(object):
+    """
+    A Trainer Class for test & evaluate model :)
+    """
     def __init__(self, model, config, name: str, device: str):
+        """
+        :param model:
+        :param config:
+        :param name:
+        :param device:
+        """
         self.name = name
         self.config = config
         self.device = device
@@ -24,15 +38,162 @@ class Trainer(object):
 
         self.model = model
         self.model.to(self.device)
+        self.writer = ts.SummaryWriter(f"runs/{self.name}_{self.now}")
         self._get_optimizer()
         self._get_criterion()
         self._resume()
 
-    def _train(self):
-        pass
+    def _overfit(self, model, epoch, batch):
+        """
+        overfit test :)
+        :param model: model
+        :param epoch: current epoch
+        :param batch: batch to overfit
+        :return:
+        """
+        model.train()
+        loss_total = AverageMeter()
+        semantic_total = AverageMeter()
+        aux_total = AverageMeter()
+        metric = ConfusionMatrix(num_classes=self.config.NUM_CLASSES)
+        set_seed(epoch)
+        inputs, targets = batch[0].to(self.device), batch[1].to(self.device)
+        with torch.cuda.amp.autocast(enabled=self.scaler.is_enabled()):
+            outputs = model(inputs)
+            semantic_loss, semantic_aux = self._criterion_forward(outputs, targets)
+            loss = semantic_loss + (0.4 * semantic_aux)
+            metric.update(targets.flatten(), outputs["out"].argmax(dim=1).flatten())
+        self.optimizer.zero_grad()
+        self.scaler.scale(loss).backward()
+        self.scaler.step(self.optimizer)
+        self.scaler.update()
+        if self.model_ema is not None:
+            self.model_ema.update(model)
 
-    def _test(self):
-        pass
+        loss_total.update(loss)
+        semantic_total.update(semantic_loss)
+        aux_total.update(semantic_aux)
+        torch.cuda.empty_cache()
+        print(f"Train -> Epoch:{epoch} Loss:{loss_total.avg:.4}"
+              f" Out Loss: {semantic_total.avg:.4} Aux Loss: {0.4 * aux_total.avg:.4},"
+              f" LR: {get_lr(self.optimizer)}")
+
+    def _train(self, model, epoch, dataloader):
+        # set model for train
+        model.train()
+
+        # save losses of each step
+        loss_total = AverageMeter()
+        semantic_total = AverageMeter()
+        aux_total = AverageMeter()
+
+        # Confusion matrix to calculate iou
+        metric = ConfusionMatrix(num_classes=self.config.NUM_CLASSES)
+        set_seed(epoch - 1)
+        loop = tqdm.tqdm(dataloader, total=len(dataloader))
+
+        # one epoch loop
+        for batch_idx, (inputs, targets) in enumerate(loop):
+            inputs, targets = inputs.to(self.device), targets.to(self.device)
+            with torch.cuda.amp.autocast(enabled=self.scaler.is_enabled()):
+
+                # forward step & calc loss & metric
+                outputs = model(inputs)
+                semantic_loss, semantic_aux = self._criterion_forward(outputs, targets)
+                loss = semantic_loss + (0.4 * semantic_aux)
+
+                # gradient accumulation
+                if self.config.GRADIENT_ACCUMULATION_STEPS > 1:
+                    loss = loss / self.config.GRADIENT_ACCUMULATION_STEPS
+                metric.update(targets.flatten(), outputs["out"].argmax(dim=1).flatten())
+
+            # backward step
+            self.scaler.scale(loss).backward()
+            if (batch_idx + 1) % self.config.GRADIENT_ACCUMULATION_STEPS == 0:
+
+                # if you use gradient accumulation i suggest you to use gradient norm  to prevent gradient exploding
+                if self.config.GRADIENT_NORM is not None:
+                    self.scaler.unscale_(self.optimizer)
+                    if hasattr(model, "get_params"):
+                        params = model.get_params(lr=self.config.LR, weight_decay=self.config.WEIGHT_DECAY)
+                    else:
+                        params = model.parameters()
+                    nn.utils.clip_grad_norm_(params, self.config.GRADIENT_NORM)
+
+                self.scaler.step(self.optimizer)
+                self.scaler.update()
+                if self.model_ema is not None:
+                    self.model_ema.update(model)
+                self.optimizer.zero_grad()
+
+                loss_total.update(loss * self.config.GRADIENT_ACCUMULATION_STEPS)
+                semantic_total.update(semantic_loss)
+                aux_total.update(semantic_aux)
+
+            torch.cuda.empty_cache()
+            loop.set_description(f"Train -> Epoch:{epoch} Loss:{loss_total.avg:.4}"
+                                 f" Out Loss: {semantic_total.avg:.4} Aux Loss: {0.4 * aux_total.avg:.4},"
+                                 f" LR: {get_lr(self.optimizer)}")
+        self.scheduler.step()
+        miou = metric.calculate()
+
+        state = {
+            'model': model.state_dict(),
+            'acc': miou,
+            'epoch': epoch,
+            'scaler': self.scaler.state_dict(),
+            'optimizer': self.optimizer.state_dict(),
+            'scheduler': self.scheduler.state_dict(),
+        }
+        if self.model_ema is not None:
+            state.update({
+                "ema": self.model_ema.state_dict(),
+                "model_ema": self.model_ema.module.state_dict()
+            })
+        path = f"{self.name}" + '.pth'
+        torch.save(state, path)
+
+        return miou, semantic_total.avg.item()
+
+    def _test(self, model, epoch, dataloader):
+        # use Reparameterize model or Original model
+        if epoch % 5 == 0:
+            print("Reparameterize model")
+            rmodel = reparameterize_model(model)
+        else:
+            print("Original model")
+            rmodel = copy.deepcopy(model)
+
+        # set model for train
+        rmodel.eval()
+
+        # save losses of each step
+        loss_total = AverageMeter()
+        semantic_total = AverageMeter()
+        aux_total = AverageMeter()
+
+        # Confusion matrix to calculate iou
+        metric = ConfusionMatrix(num_classes=self.config.NUM_CLASSES)
+        set_seed(epoch - 1)
+        loop = tqdm.tqdm(dataloader, total=len(dataloader))
+
+        # one epoch loop
+        for batch_idx, (inputs, targets) in enumerate(loop):
+            inputs, targets = inputs.to(self.device), targets.to(self.device)
+            with torch.cuda.amp.autocast(enabled=self.scaler.is_enabled()):
+                outputs = rmodel(inputs)
+                semantic_loss, semantic_aux = self._criterion_forward(outputs, targets)
+                loss = semantic_loss + (0.4 * semantic_aux)
+                metric.update(targets.flatten(), outputs["out"].argmax(dim=1).flatten())
+            loss_total.update(loss)
+            semantic_total.update(semantic_loss)
+            aux_total.update(semantic_aux)
+            torch.cuda.empty_cache()
+            loop.set_description(f"Valid -> Epoch:{epoch} Loss:{loss_total.avg:.4}"
+                                 f" Out Loss: {semantic_total.avg:.4} Aux Loss: {0.4 * aux_total.avg:.4}")
+        miou = metric.calculate()
+
+        return miou, semantic_total.avg.item()
 
     def _save(self, acc, epoch=-1):
         if acc > self.best_acc:
@@ -56,7 +217,7 @@ class Trainer(object):
             self.best_acc = acc
 
     def _resume(self):
-        self.start_epoch, self.best_acc = -1, 0.0
+        self.start_epoch, self.best_acc = 0, 0.0
         if os.path.isfile(f"{self.name}" + '.pth'):
             try:
                 loaded = torch.load(f"{self.name}" + '.pth')
@@ -80,16 +241,16 @@ class Trainer(object):
 
     def _get_optimizer(self):
         if hasattr(self.model, "get_params"):
-            params = self.model.get_params(lr=self.config.ADAMW_LR, weight_decay=self.config.WEIGHT_DECAY)
+            params = self.model.get_params(lr=self.config.LR, weight_decay=self.config.WEIGHT_DECAY)
         else:
             params = self.model.parameters()
 
         if self.config.OPTIMIZER == "ADAMW":
-            self.optimizer = optim.AdamW(params, lr=self.config.ADAMW_LR, weight_decay=self.config.WEIGHT_DECAY,
+            self.optimizer = optim.AdamW(params, lr=self.config.LR, weight_decay=self.config.WEIGHT_DECAY,
                                          betas=self.config.ADAMW_BETAS)
 
         elif self.config.OPTIMIZER == "SGD":
-            self.optimizer = optim.SGD(params, lr=self.config.ADAMW_LR, weight_decay=self.config.WEIGHT_DECAY,
+            self.optimizer = optim.SGD(params, lr=self.config.LR, weight_decay=self.config.WEIGHT_DECAY,
                                        momentum=self.config.SGD_MOMENTUM)
 
         else:
@@ -112,7 +273,7 @@ class Trainer(object):
         else:
             raise NotImplemented
 
-        if self.config.WARMUP_EPOCHS > 0:
+        if self.config.WARMUP_EPOCHS > 0 and not self.config.OVERFIT_TEST:
             warm_lr_scheduler = torch.optim.lr_scheduler.LinearLR(
                 self.optimizer, start_factor=self.config.WARMUP_FACTOR, total_iters=warm_iters)
 
@@ -155,3 +316,32 @@ class Trainer(object):
             elif "aux" in k:
                 semantic_aux += self.criterion(v, target)
         return semantic_loss, semantic_aux
+
+    def fit(self, trainloader, testloader):
+
+        for epoch in range(self.start_epoch + 1, self.config.EPOCHS + 1):
+            if epoch % 5 == 0:
+                model_type = "Reparameterize"
+            else:
+                model_type = "Original"
+            train_acc, train_loss = self._train(model=self.model, epoch=epoch, dataloader=trainloader)
+            test_acc, test_loss = self._test(model=self.model, epoch=epoch, dataloader=testloader)
+            self.writer.add_scalar('Loss/train', train_loss.item(), epoch)
+            self.writer.add_scalar('Metric/train', train_acc, epoch)
+            self.writer.add_scalar('LR/train', get_lr(self.optimizer), epoch)
+            self.writer.add_scalar('Loss/valid', test_loss.item(), epoch)
+            self.writer.add_scalar('Metric/valid', test_acc, epoch)
+            if self.model_ema is not None:
+                ema_acc, ema_loss = self._test(model=self.model_ema.module, epoch=epoch, dataloader=testloader)
+                self.writer.add_scalar('Loss/valid_ema', ema_loss.item(), epoch)
+                self.writer.add_scalar('Metric/valid_ema', ema_acc, epoch)
+            else:
+                ema_acc, ema_loss = 0, 0
+
+            with open(f"{self.name}.txt", "a") as f:
+                f.write(f"Epoch: {epoch}, Model: {model_type},"
+                        f" Train mIOU: {train_acc}, Train loss: {train_loss},"
+                        f" Valid mIOU: {test_acc}, Valid loss: {test_loss},"
+                        f" EMA mIOU: {ema_acc}, EMA loss: {ema_loss}\n")
+
+            print()
